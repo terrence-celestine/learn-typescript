468e285450e3e2b1a74b894dfc0a49db27d0067d
terrence-celestine <terrencebutler89@gmail.com>
2022-08-12 13:51:49 -0700

    Setup the foundation for the project

---
+++ lesson-1-hello-world/app.js
@@ -0,0 +1,5 @@
+var Terrence = {
+    name: "Terrence",
+    age: 26
+};
+console.log(Terrence); // returns {name:"Terrence", age: 26}
---
+++ lesson-1-hello-world/app.ts
@@ -0,0 +1,11 @@
+interface Person {
+  name: string;
+  age: number;
+}
+
+const Terrence: Person = {
+  name: "Terrence",
+  age: 26,
+};
+
+console.log(Terrence); // returns {name:"Terrence", age: 26}
