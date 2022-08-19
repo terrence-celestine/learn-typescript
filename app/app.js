e6f285d82d1933983fb846e785541d8efec21a3c
terrence-celestine <terrencebutler89@gmail.com>
2022-08-19 13:05:05 -0700

    Learned about record-types

--- app/app.js
+++ app/app.js
@@ -1,91 +1,18 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Department = /** @class */ (function () {
-    function Department(id, name) {
-        this.id = id;
-        this.name = name;
-        this.employees = [];
-        this.name = name;
-        this.id = id;
-    }
-    Department.prototype.addEmployee = function (employee) {
-        this.employees.push(employee);
-    };
-    Department.prototype.printEmployeeInformation = function () {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    };
-    Department.createEmployee = function (name) {
-        return { name: name };
-    };
-    Department.prototype.describe = function () { };
-    return Department;
-}());
-var ITDepartment = /** @class */ (function (_super) {
-    __extends(ITDepartment, _super);
-    function ITDepartment(id, admins) {
-        var _this = _super.call(this, id, ": IT") || this;
-        _this.admins = admins;
-        return _this;
-    }
-    ITDepartment.prototype.describe = function () {
-        console.log("IT Department - ID:" + this.id);
-    };
-    return ITDepartment;
-}(Department));
-var AccountingDepartment = /** @class */ (function (_super) {
-    __extends(AccountingDepartment, _super);
-    function AccountingDepartment(id, reports) {
-        var _this = _super.call(this, id, "Accounting") || this;
-        _this.reports = reports;
-        _this.lastReport = reports[0];
-        return _this;
-    }
-    Object.defineProperty(AccountingDepartment.prototype, "mostRecentReport", {
-        get: function () {
-            if (this.lastReport) {
-                return this.lastReport;
-            }
-            throw new Error("No report found");
-        },
-        set: function (value) {
-            if (!value) {
-                throw new Error("Please pass in a valid value");
-            }
-            this.addReport(value);
-        },
-        enumerable: false,
-        configurable: true
-    });
-    AccountingDepartment.prototype.addReport = function (text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    };
-    AccountingDepartment.prototype.printReports = function () {
-        console.log(this.reports);
-    };
-    AccountingDepartment.prototype.describe = function () {
-        console.log("Accoting Department - ID:" + this.id);
-    };
-    return AccountingDepartment;
-}(Department));
-var employee1 = Department.createEmployee("Max");
-var IT = new ITDepartment("d1", ["Terrence"]);
-var accounting = new AccountingDepartment("d2", []);
-accounting.mostRecentReport = "Year End Report";
-console.log(accounting.mostRecentReport);
-accounting.addEmployee("Peter");
-accounting.printEmployeeInformation();
+var zWarriors = new Map();
+zWarriors.set("goku", {
+    race: "saiyan",
+    abilities: ["kamehameha", "kaioken"]
+});
+zWarriors.set("krillin", {
+    race: "human",
+    abilities: ["solar flare", "quack"]
+});
+zWarriors.set("piccolo", {
+    race: "namekian",
+    abilities: ["solar flare", "special beam cannon"]
+});
+zWarriors.set("vegeta", {
+    race: "saiyan",
+    abilities: ["great ape", "galick gun"]
+});
+console.log(zWarriors.keys());
